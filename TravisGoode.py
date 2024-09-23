6d5d7dd4f49608d811abeac3657468c7dd94cb20
Axino13 <Goode.travis@yahoo.com>
2024-09-22 17:12:23 -0700

    Add files via upload

---
+++ TravisGoode.py
@@ -0,0 +1,14 @@
+# Travis Goode
+# 9/22/24
+# P1LAB1
+# This will grab and user input to display a welcome message
+
+
+first_name = input("Enter your first name: ")
+
+
+last_name = input("Enter your last name: ")
+
+
+# Print
+print(f"Hello, {first_name} {last_name}, Welcome to CTI-110!")
